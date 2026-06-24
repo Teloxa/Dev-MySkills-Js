74b7cf794ccca906848fbc67d7a665f7c7d85d5d
Teloxa <moralesdvd03@hotmail.com>
2026-06-23 22:55:48 -0600

    task: Closure's introduction little description TODO: examples

--- 00-Fundamentals-js/closures.js
+++ 00-Fundamentals-js/closures.js
@@ -1,1 +1,13 @@
-// 00-Fundamentals-js/closures.js
+// 00-Fundamentals-js/closures.js 
+// They are scope + first-class functions. A closure is a function that has access to its own scope, the outer function's scope, and the global scope.
+
+function makeCounter() {
+    let count = 0; // This variable lives in the closure
+    return {
+        increment: () => ++count,
+        value: () => count
+    }
+}
+
+// Each call to 'makeCounter' creates a separate scope.
+// React hooks sue this exact mechanism 'useState' is a clousure around the state.
